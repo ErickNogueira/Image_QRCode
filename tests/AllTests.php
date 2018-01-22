23ecea94dea3e40a4ddbcaacf9a2ea7efebd0040
Rich Sage <rich.sage@gmail.com>
2018-01-22 14:08:45 +0000

    Tidy tests, ensure they work with latest phpunit.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,32 +1,17 @@
 <?php
 /**
  * QRCode_AllTests
- * 
+ *
  * @category  Images
  * @package   Image_QRCode
- * @author    Rich Sage <rich.sage@gmail.com> 
+ * @author    Rich Sage <rich.sage@gmail.com>
  * @copyright 2009 Rich Sage
- * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1 
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @link      http://code.google.com/p/pearqrcode
  */
 
-if ($fp = @fopen('PHPUnit/Autoload.php', 'r', true)) {
-    require_once 'PHPUnit/Autoload.php';
-} elseif ($fp = @fopen('PHPUnit/Framework.php', 'r', true)) {
-    require_once 'PHPUnit/Framework.php';
-} else {
-    die('skip could not find PHPUnit');
-}
-fclose($fp);
-
-// Keep tests from running twice when calling this file directly via PHPUnit.
-$call_main = false;
-if (strpos($_SERVER['argv'][0], 'phpunit') === false) {
-    // Called via php, not PHPUnit.  Pass the request to PHPUnit.
-    if (!defined('PHPUnit_MAIN_METHOD')) {
-        define('PHPUnit_MAIN_METHOD', 'QRCode_AllTests::main');
-        $call_main = true;
-    }
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'QRCode_AllTests::main');
 }
 
 require_once 'Image/QRCode.php';
@@ -34,37 +19,34 @@ require_once 'Image/QRCodeTest.php';
 
 /**
  * All tests for Image_QRCode
- * 
+ *
  * @category  Images
  * @package   Image_QRCode
- * @author    Rich Sage <rich.sage@gmail.com> 
+ * @author    Rich Sage <rich.sage@gmail.com>
  * @copyright 2009 Rich Sage
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @link      http://code.google.com/p/pearqrcode
  */
-class QRCode_AllTests extends PHPUnit_Framework_TestSuite
+class QRCode_AllTests extends \PHPUnit\Framework\TestSuite
 {
     public static function main()
     {
-        if (!function_exists('phpunit_autoload')) {
-            require_once 'PHPUnit/TextUI/TestRunner.php';
-        }
-        PHPUnit_TextUI_TestRunner::run(self::suite());
+        \PHPUnit\TextUI\TestRunner::run(self::suite());
     }
 
     /**
-     * suite 
-     * 
-     * @return PHPUnit_Framework_TestSuite
+     * suite
+     *
+     * @return \PHPUnit\Framework\TestSuite
      */
     static public function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('Image_QRCode Unit Test Suite');
+        $suite = new \PHPUnit\Framework\TestSuite('Image_QRCode Unit Test Suite');
         $suite->addTestSuite('Image_QRCodeTest');
         return $suite;
     }
 }
 
-if ($call_main) {
+if (PHPUnit_MAIN_METHOD == 'QRCode_AllTests::main') {
     QRCode_AllTests::main();
 }
